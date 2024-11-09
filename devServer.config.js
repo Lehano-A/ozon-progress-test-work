445793ba9a62fd221864670bef83bc4b95a622b3
Lehano-A <48220644+Lehano-A@users.noreply.github.com>
2024-11-09 14:42:26 +0300

    Добавляет файлы и пакеты для разработки

---
+++ devServer.config.js
@@ -0,0 +1,16 @@
+
+const path = require('path');
+
+const config = {
+  static: {
+    directory: path.join(__dirname, '/'),
+  },
+
+  compress: true,
+  port: 8080,
+  client: {
+    overlay: true,
+  },
+}
+
+module.exports = { config }
