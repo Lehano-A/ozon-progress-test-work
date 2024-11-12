b211fd1cf4ae483e22c2c055c61a1540debde808
Lehano-A <48220644+Lehano-A@users.noreply.github.com>
2024-11-12 08:16:31 +0300

    Добавляет входной index.js

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import "../index.css";
+
+import "./components/progress-indicator/progressIndicator";
+import "./components/controls/switch/switch";
+import "./components/controls/control-value/controlValue";
